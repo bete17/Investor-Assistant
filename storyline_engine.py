202ba80141b3e84d86f4814d9f4f156b22e162ff
bruce <brucetan@berkeley.edu>
2026-07-25 13:20:30 -0700

    set up skeleton code

---
+++ storyline_engine.py
@@ -0,0 +1,28 @@
+import os
+
+def text_diff(text1: str, text2: str) -> str:
+    """Return the difference between two texts
+
+    Args:
+        text1 (str): first text
+        text2 (str): second text
+    """
+    pass
+
+def reduce_text(tenK: str):
+    """pass the text through text diff to reduce the text to only the differences
+
+    Args:
+        tenK (str): the 10-k filing in html format
+    """
+    pass
+
+def save_text(tenK: str, ticker: str, date):
+    """Save the text to a file
+
+    Args:
+        tenK (str): the 10-k filing in html format
+        ticker (str): company's ticker
+        date (str): date of the 10-k filing
+    """
+    pass
