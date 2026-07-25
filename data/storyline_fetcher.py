a73093f318a7f4dc5a70bc02b1853f189e8eaaa0
bruce <brucetan@berkeley.edu>
2026-07-25 13:20:30 -0700

    set up skeleton code

---
+++ data/storyline_fetcher.py
@@ -0,0 +1,25 @@
+import os
+
+def get_tenK(ticker: str, date):
+    """Fetch the 10-k
+
+    Args:
+        ticker (str): company's ticker
+        date (str): date of the 10-k filing
+    """
+    pass
+    
+def find_item7(tenK):
+    """Find the item 7 in the 10-k
+
+    Args:
+        tenK (str): the 10-k filing in html format
+    """
+    pass
+
+def upload_items(filepath: str):
+    """Upload the items to cache
+    Args:
+        file_path (str): path to the file containing the items
+    """
+    pass
