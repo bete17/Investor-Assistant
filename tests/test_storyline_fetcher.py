af7e66e706b75e5f194c26573a9688505ead3e02
bruce <brucetan@berkeley.edu>
2026-07-29 16:25:14 -0700

    created files for storyline testing

---
+++ tests/test_storyline_fetcher.py
@@ -0,0 +1,10 @@
+"""
+Things to test:
+- Is it able to fetch the data from the API?
+- Is it able to save the data to cache?
+- Is it the right 10k filing?
+- Is it able to locate item 7
+- Is the item 7 data correct?
+- Is the item 7 data complete?
+- Is it able to save the data to cache correctly?
+"""
